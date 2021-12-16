3271ceab623e1f737c7291302def20f2fc370e59
seonja <seonja.kim0@gmail.com>
2021-12-16 19:41:49 +0900

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <>
       <a href="http://52.78.177.198:8080/oauth2/authorization/seoul42 ">
-        <div>hello</div>
+        <button style={{ padding: '1em' }}>로그인</button>
       </a>
       <Link to="/project">
         <button>project</button>
