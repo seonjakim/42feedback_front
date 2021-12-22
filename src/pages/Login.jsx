38ff2c48b69042436fad447847d19e0bf9e7b33a
seonja <seonja.kim0@gmail.com>
2021-12-22 20:19:26 +0900

    login page with rough frame

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { StyledFullButton } from '../components/buttons/StyledButton'
 
 const Login = () => {
   return (
-    <>
+    <StyledContainer>
+      <div className="center">
+        <h1>42 Feedback</h1>
+        <h3>프로젝트 후, 건설적인 피드백이 필요하신 모든 카뎃</h3>
+      </div>
       <a href="https://52.78.177.198:8080/oauth2/authorization/seoul42 ">
-        <button style={{ padding: '1em' }}>로그인</button>
+        <StyledFullButton emphasis="high">42에서 로그인하기</StyledFullButton>
       </a>
-      <Link to="/project">
-        <button>project</button>
-      </Link>
-      <button>cadet</button>
-    </>
+    </StyledContainer>
   )
 }
 
 export default Login
+
+const StyledContainer = styled.div`
+  display: grid;
+  height: 100%;
+  grid-template-rows: 1fr auto;
+
+  & .center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 4em;
+
+    h1 {
+      padding: 1em;
+    }
+    h3 {
+      color: ${({ theme }) => theme.font.grey};
+    }
+  }
+`
