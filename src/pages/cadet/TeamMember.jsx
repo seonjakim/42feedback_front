7a54a1ddd5714930e868b7ed45d0a91f25817edd
seonja <seonja.kim0@gmail.com>
2021-12-26 10:41:08 +0900

    team memeber list api data change

--- src/pages/cadet/TeamMember.jsx
+++ src/pages/cadet/TeamMember.jsx
@@ -6,21 +6,25 @@ import CadetCard from '../../components/cadetCard/CadetCard'
 import { fetchData } from '../../library/index'
 
 const TeamMember = () => {
-  const [teamMember, setTeamMember] = React.useState([])
+  const [teamMember, setTeamMember] = React.useState({
+    userId: null,
+    appraisedUserFeedbackList: [],
+  })
   const { id } = useParams()
   React.useEffect(() => {
     fetchData(`/project/${id}/user/feedback-list`, setTeamMember)
   }, [])
   console.log(teamMember)
+  const { userId, appraisedUserFeedbackList } = teamMember
   return (
     <div>
       <Header title="팀원 리스트" />
       <div style={{ marginTop: '2em' }}>
-        {teamMember.map((member, index) => (
+        {appraisedUserFeedbackList.map((member, index) => (
           <div style={{ margin: '4px 8px' }} key={index}>
             <CadetCard
               member={member}
-              url={`/cadet/${teamMember.userId}/${id}/${member.userId}/feedback/${member.login}`}
+              url={`/cadet/${userId}/${id}/${member.userId}/feedback/${member.login}`}
             />
           </div>
         ))}
